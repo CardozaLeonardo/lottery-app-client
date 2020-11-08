c68c59d0bce4e113b7edd9ed553f2118e1335bb6
Leonardo <leonardoorozco69@gmail.com>
2020-11-08 13:16:13 -0600

    Footer and new bg color

---
+++ components/layout/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+
+    return (
+        <footer className="bg-bg3 text-gray-400 px-4 py-6">
+            <h3>This is the footer!</h3>
+        </footer>
+    )
+}
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,11 @@ module.exports = {
     extend: {
       colors: {
         'primary':' #3563BD',
-        'secondary': '#3563BD',
+        'secondary': '#4568E4',
         'gray1':'#BBB4B4',
         'active': '#FF992F',
         'bg2':' #222243',
+        'bg3':'#0A0A27',
       }
     },
   },
