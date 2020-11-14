16cade65937c8ac1bf552c900669b10fa173b9e0
Leonardo <leonardoorozco69@gmail.com>
2020-11-13 18:15:01 -0600

    New bg colors and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,13 @@ module.exports = {
         'secondary': '#4568E4',
         'gray1':'#BBB4B4',
         'active': '#FF992F',
+        'bg1':'#393857',
         'bg2':' #222243',
         'bg3':'#0A0A27',
+      },
+
+      spacing : {
+        'sd':'300px'
       }
     },
   },
