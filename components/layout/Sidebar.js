910c1edd8f5f4156e8009fa463dc0febc6bcb84b
Leonardo <leonardoorozco69@gmail.com>
2020-11-13 18:16:04 -0600

    Sidebar options

--- components/layout/Sidebar.js
+++ components/layout/Sidebar.js
@@ -3,7 +3,7 @@ const Sidebar = ({children}) => {
 
     return (
 
-        <div className="bg-bg2 w-1/6 px-4" >
+        <div className="bg-bg2 w-sd px-4" >
             { children }
         </div>
     )
---
+++ components/shared/SidebarOption.js
@@ -0,0 +1,14 @@
+
+const SidebarOption = ({action, children, active}) => {
+
+    const bgActive = active ? 'bg-blue-900':'';
+
+    return (
+        <div onClick={() => action()} className={`flex py-3 px-2 text-gray-300 text-sm rounded-xl 
+        hover:bg-gray-500 cursor-pointer items-center ${bgActive}`}>
+            {children}
+        </div>
+    )
+}
+
+export default SidebarOption;
