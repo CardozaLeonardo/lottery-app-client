4de60780c6f03e555095bf7ab1394efd4de5ba35
Leonardo <leonardoorozco69@gmail.com>
2020-12-02 02:39:45 -0600

    Custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+
+const Custom404 = () => {
+
+    return (
+        <div className="w-full flex justify-center items-center" style={{height: '100vh'}}>
+            <h1 className="text-gray-200 text-2xl">There is nothing here!</h1>
+        </div>
+    )
+}
+
+export default Custom404;
