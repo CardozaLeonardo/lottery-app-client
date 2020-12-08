97513fc82e0f866c5b6658cd3abfb79ab8821912
Leonardo <leonardoorozco69@gmail.com>
2020-12-08 11:55:02 -0600

    Focus outline none on inputs

--- components/form/Input.js
+++ components/form/Input.js
@@ -3,7 +3,7 @@ const Input = ({type="text",placeholder, callback, value="", name}) => {
 
     return(
         <input value={value} type={type} className="text-gray-400 w-full bg-transparent border-b-2 
-        border-gray-600 border-opacity-50 text-opacity-50" onChange={callback} id={name} name={name}/>
+        border-gray-600 border-opacity-50 text-opacity-50 focus:outline-none" onChange={callback} id={name} name={name}/>
     )
 }
 
